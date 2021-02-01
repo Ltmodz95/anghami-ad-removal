9fa2581282432e3e4aafa1b93d632aaeaec31197
Ltmodz95 <mohamed.a.hussein.elsayed@gmail.com>
2021-02-01 12:20:43 +0200

    Enchancing the mutatition observer to only handle the body changes
    *Purpose of change:
     -Mutation observer sometimes go into an infinite loop that makes the app crash
     -Play button was not being clicked automaticaliy
    
    *Changes that were made:
     -Changing the mutation observer configs to only listen to the body tag chiled not all the subtrees
     -removing functions that are no longer used
     -Setting a timeout of 1s to avoid the video ad and the music player from raising an exception when pressing play
     -updating the manifest file

--- scripts/contentScripts/adRemover.js
+++ scripts/contentScripts/adRemover.js
@@ -1,66 +1,55 @@
 const targetToObserve = document.querySelector('body');
-const config = {attributes: false, childList: true, subtree: true };
-let autoPlay = true;
-const callback = function(mutationsList) {
-    for(const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            googleIframesRemover();//removing google frames
-            adsContainerRemover(); //removing the ads container
-            adsModalBackDropRemover(); // removing the backdrop of the modal to make the app interactive again
-            adsModalWindowRemover(); // removing the main ads container
-            pressPlay();  // press the play button again cause am too lazy to press it again.
-           
-        }
-
-    }
+const config = {
+    attributes: false,
+    childList: true,
+    subtree: false
 };
 
-const observer = new MutationObserver(callback);
-observer.observe(targetToObserve, config);
-
-
-//helper functions
-
-const googleIframesRemover = () =>{
-    const googleIframes = Array.prototype.filter.call(document.querySelectorAll("iframe"),x=>x.name.includes('google'))
-    removeItems(googleIframes,"removed google iframe")
-}
-
-const adsContainerRemover = ()=>
-{
-    removeItems(document.querySelectorAll("anghami-ads"),"removed ad container")
-}
-
-const adsModalBackDropRemover = ()=>{
-    removeItems(document.querySelectorAll("ngb-modal-backdrop"),"removed the modal popup back drop")
-}
-
-const adsModalWindowRemover = ()=>{
-    const adsModals = document.querySelectorAll("ngb-modal-window");
-    if(adsModals.length)
-    {
-        autoPlay = true;
-    }
-    removeItems(document.querySelectorAll("ngb-modal-window"),"removed the modal popup ")
-}
-
-const pressPlay = ()=>{
-    const playButton = document.querySelector(".play-pause-cont");
-    if(playButton && playButton.querySelector(".play") && autoPlay)
-    {
-        playButton.click();
-        if(playButton.querySelector(".pause"))
-        {
-            autoPlay = !autoPlay;
+const callback = function (mutationsList) {
+    for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+            googleIframesRemover();
+            pressPlay();
+            muteVideoAds();
+            hideAdModals();
         }
-       
+    }
+};
+const observer = new MutationObserver(callback);
+document.addEventListener('readystatechange', event => {
+    if (event.target.readyState === 'complete') {
+        observer.observe(targetToObserve, config);
+    }
+});
+//helper functions
+const googleIframesRemover = () => {
+    const googleIframes = Array.prototype.filter.call(document.querySelectorAll("iframe"), x => x.name.includes('google'))
+    removeItems(googleIframes, "removed google iframe")
+}
+const muteVideoAds = () => {
+    const videoAds = document.querySelectorAll("video");
+    removeItems(videoAds);
+    setTimeout(pressPlay, 1000);
+}
+const hideAdModals = () => {
+    const adModals = document.querySelectorAll("ngb-modal-window");
+    const adModalsBackDrop = document.querySelectorAll("ngb-modal-backdrop");
+    hideItems([...adModals, ...adModalsBackDrop]);
+    console.log("removed an ad");
+}
+const pressPlay = () => {
+    const playButton = document.querySelector(".play-pause-cont");
+    if (playButton && playButton.querySelector(".play")) {
+        playButton.click();
     }
 }
-
-const removeItems = (items,desc) =>
-{
-    for(const item of items){
+const removeItems = (items) => {
+    for (const item of items) {
         item.parentNode.removeChild(item);
-            console.log(`${desc}`);
+    }
+}
+const hideItems = (items) => {
+    for (const item of items) {
+        item.style.visibility = 'hidden';
     }
 }
