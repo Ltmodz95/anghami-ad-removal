d89852c12b577c427da1b499bba0fc456c3187fb
IbrahimAzab <54166348+simplehima@users.noreply.github.com>
2023-12-18 09:34:18 +0200

    Update adRemover.js

--- scripts/contentScripts/adRemover.js
+++ scripts/contentScripts/adRemover.js
@@ -1,69 +1,77 @@
-const targetToObserve = document.querySelector('body');
+const targetToObserve = document.body;
 const config = {
-    attributes: false,
-    childList: true,
-    subtree: false
+  childList: true,
 };
 
-const callback = function (mutationsList) {
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            googleIframesRemover();
-            removeAdContainers();
-            pressPlay();
-            muteVideoAds();
-            removeLeaderboard();
-        }
+const callback = (mutationsList) => {
+  for (const mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      optimizePage();
     }
+  }
 };
+
 const observer = new MutationObserver(callback);
-document.addEventListener('readystatechange', event => {
-    if (event.target.readyState === 'complete') {
-        observer.observe(targetToObserve, config);
-    }
-});
-//helper functions
-const googleIframesRemover = () => {
-    const googleIframes = Array.prototype.filter.call(document.querySelectorAll("iframe"), x => x.name.includes('google'))
-    removeItems(googleIframes, "removed google iframe")
-}
-const muteVideoAds = () => {
-    for(const videoAd of document.querySelectorAll("#native-ad-video"))
-    {
-        videoAd.onplay = (e)=>{
-            console.log("played",e);
-            e.target.muted = true;
-            hideAdModals();
-            pressPlay()
-        }
-    }
-}
-const hideAdModals = () => {
-    const adModals = document.querySelectorAll("ngb-modal-window");
-    const adModalsBackDrop = document.querySelectorAll("ngb-modal-backdrop");
-    document.body.className = document.body.className.replace("modal-open","");
-    hideItems([...adModals, ...adModalsBackDrop]);
-    console.log("removed an ad");
-}
-const removeLeaderboard = () => {
-    var leaderboard = document.querySelector('.player-leaderboard');
-    leaderboard.parentNode.removeChild(leaderboard);
-}
-const pressPlay = () => {
-    const playButton = document.querySelector(".play-pause-cont");
-    if (playButton && playButton.querySelector(".play")) {
-        playButton.click();
-    }
 
+document.addEventListener("readystatechange", (event) => {
+  if (event.target.readyState === "complete") {
+    observer.observe(targetToObserve, config);
+  }
+});
+
+function optimizePage() {
+  googleIframesRemover();
+  removeAdContainers();
+  pressPlay();
+  muteVideoAds();
+  removeLeaderboard();
 }
-const removeAdContainers = () => removeItems(document.querySelectorAll("anghami-ads"));
-const removeItems = (items) => {
-    for (const item of items) {
-        item.parentNode.removeChild(item);
-    }
+
+function googleIframesRemover() {
+  const googleIframes = Array.from(
+    document.querySelectorAll("iframe[name*='google']")
+  );
+  removeItems(googleIframes, "removed google iframe");
 }
-const hideItems = (items) => {
-    for (const item of items) {
-        item.style.visibility = 'hidden';
-    }
+
+function muteVideoAds() {
+  document.querySelectorAll("#native-ad-video").forEach((videoAd) => {
+    videoAd.addEventListener("play", (e) => {
+      console.log("played", e);
+      e.target.muted = true;
+      hideAdModals();
+      pressPlay();
+    });
+  });
+}
+
+function hideAdModals() {
+  const adModals = document.querySelectorAll(
+    "ngb-modal-window, ngb-modal-backdrop"
+  );
+  document.body.classList.remove("modal-open");
+  hideItems(adModals);
+  console.log("removed an ad");
+}
+
+function removeLeaderboard() {
+  const leaderboard = document.querySelector(".player-leaderboard");
+  if (leaderboard) leaderboard.remove();
+}
+
+function pressPlay() {
+  const playButton = document.querySelector(".play-pause-cont .play");
+  if (playButton) playButton.click(); // Dispatching 'click' may not work as expected in some cases
+}
+
+function removeAdContainers() {
+  removeItems(document.querySelectorAll("anghami-ads"));
+}
+
+function removeItems(items) {
+  items.forEach((item) => item.remove());
+}
+
+function hideItems(items) {
+  items.forEach((item) => (item.style.visibility = "hidden"));
 }
